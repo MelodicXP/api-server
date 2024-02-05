c65f154cd2313dca7416a59423f4f86fdb4b366d
MELODICXP <CARMELOGONZALEZ1312@GMAIL.COM>
2024-02-05 08:42:54 -0600

    Add CRUD operations for book

--- src/routes/book.js
+++ src/routes/book.js
@@ -4,89 +4,89 @@
 const express = require('express');
 
 // Import clothesModel from models/index
-const { clothesModel } = require('../models/index');1
+const { bookCollection } = require('../models/index');
 
 // Single instance of router
 const router = express.Router();
 
-// POST items to database, connect to database to create first, then send back data added as confirmation, else catch error
-router.post('/clothes', async (req, res, next) => {
+// ** POST book to database, connect to database to create first, then send back data added as confirmation, else catch error
+router.post('/book', async (req, res, next) => {
   try {
     // Takes in object (req.body) as an argument for .create()
-    const newClothesItem = await clothesModel.create(req.body);
-    res.status(200).send(newClothesItem);
+    const newBook = await bookCollection.create(req.body);
+    res.status(200).send(newBook);
   } catch(e) {
+    console.error('Error getting all books:', e);
     next(e);
   }
 });
 
-// GET all clothes items from database, await connection to database and send back data
-router.get('/clothes', async (req, res, next) => {
-  const clothes = await clothesModel.findAll();
-  res.status(200).send(clothes);
+// ** GET all books items from database, await connection to database and send back data
+router.get('/book', async (req, res, next) => {
+  const books = await bookCollection.read();
+  res.status(200).send(books);
 });
 
-// GET one item by id
-router.get('/clothes/:id', async (req, res, next) => {
+// ** GET one book by id
+router.get('/book/:id', async (req, res, next) => {
   try {
     // set id variable from request to find by id
     const id = req.params.id;
-    // find item by id 
-    const clothesItem = await clothesModel.findOne({ where: { id } });
+    // find book by id 
+    const book = await bookCollection.read(id);
 
-    if (!clothesItem) {
-      // item not found, return a 404 status
-      return res.status(404).send({ message: `Clothes item with ID ${id} not found` });
+    if (!book) {
+      // book not found, return a 404 status
+      return res.status(404).send({ message: `Book with ID ${id} not found` });
     }
     
-    // item found, return it
-    res.status(200).send(clothesItem);
+    // book found, return it
+    res.status(200).send(book);
   } catch (e) { // catch error
     next(e);
   }
 });
 
-// Update a food item record
-router.put('/clothes/:id', async (req, res, next) => {
+// ** Update a food item record
+router.put('/book/:id', async (req, res, next) => {
   try {
     // set id variable from request to find food item by id
     const id = req.params.id;
 
-    // check if any rows were updated, if not handle as error case, else update food item with data sent by user in req.body to specified id
-    const [updateCount, updatedClothesItems] = await clothesModel.update(req.body, { 
-      where: { id },
-      returning: true, // returns array with two elements (num of rows affected, updated records as objects)
-    });
+    // await update, pass in id and req.body as parameters
+    const updatedBook = await bookCollection.update(id, req.body);
 
-    if (updateCount === 0) {
-      // now rows updated, so item does not exist
-      return res.status(404).send({ message: `No clothes item found with ID ${id}`});
+    // if no updatedbook data, return 404
+    if (!updatedBook) {
+      return res.status(404).send({ message: `No Author found with ID ${id}` });
     }
+
     // Send back updated item
-    res.status(200).send(updatedClothesItems[0]);
+    res.status(200).send(updatedBook);
   } catch (e) { // catch error
+    console.error('Error updating book by id:', e);
     next(e);
   }
 });
 
 // Delete a food item record
-router.delete('/clothes/:id', async (req, res, next) => {
+router.delete('/book/:id', async (req, res, next) => {
   try {
     const id = req.params.id;
-    // First, find the record to ensure it exists
-    const clothesItem = await clothesModel.findOne({ where: { id } });
 
-    if (clothesItem) {
-      // If found, delete the record
-      await clothesModel.destroy({ where: { id } });
+    // Utitlize delete method from Collection class
+    const numDeleted = await bookCollection.delete(id);
 
-      // After deletion, you can send back a response indicating the record is now null
-      res.status(200).send({ id: id, deleted: true, record: null });
+    // Check if any author was deleted
+    if (numDeleted > 0) {
+      // Author found and deleted
+      res.status(200).send({ id: id, deleted: true });
     } else {
-      // Return a 404 response specifically for this resource-not-found case
-      res.status(404).send({ message: `Clothes item with ID ${id} not found` });
+      // No authorfound with id
+      res.status(404).send({ message: `Book with ID ${id} not found` });
     }
   } catch (e) {
+    console.error('Error deleting book by id:', e);
     next(e);
   }
 });
